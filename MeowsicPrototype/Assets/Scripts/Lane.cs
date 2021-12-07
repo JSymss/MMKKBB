7b6d8d58fe7cd5d4d2eb19718a59c6c92578a6ec
Nora Luck-Jennings <1018949@student.sae.edu.au>
2021-12-07 15:30:37 +1100

    Added new SFX for gameplay

--- MeowsicPrototype/Assets/Scripts/Lane.cs
+++ MeowsicPrototype/Assets/Scripts/Lane.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59d4da2bb9d4e02f0c3579ea56000fe95cc729b87f3bd4da2a58f0f049b707b7
-size 56614
+oid sha256:d95dcf8ba51fa2417425b2a0251c1ff4af6e60dd238453f299a3c3ecc0f7f34e
+size 58255
--- MeowsicPrototype/Assets/Scripts/ScoreManager.cs
+++ MeowsicPrototype/Assets/Scripts/ScoreManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4613c3a578e9f49f1d388e2e9b50748cc8c7624739d0053e9f057a84065847d5
-size 6476
+oid sha256:4a2dc999a3b1b44a3fada5e4dd105d0fc644fa8caae3166903e164ada6360f37
+size 6706
