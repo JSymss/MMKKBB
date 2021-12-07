da65bb9aa5272ef9755ff6992462b30467a0eca5
Jackson <200341@student.sae.edu.au>
2021-12-07 18:05:01 +1100

    Animations first stage implementation (Digger in MovedOn scene)

--- MeowsicPrototype/Assets/Scripts/CatScript.cs
+++ MeowsicPrototype/Assets/Scripts/CatScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a96e2b4e82e167089a6f2cf1589ead5e3cb3d8abf88e08be4d8ad9d7df324bb1
-size 10237
+oid sha256:b61d3d2526e2726f11aad91672fd33fa1febf054bd52ded90ee556e65ef57b48
+size 11450
